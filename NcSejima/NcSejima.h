00c71374ffc8c142c1b1b8e8bffdef282fba44e8
Loïc GRIFFIE <lgriffie@MacBook-Pro-de-Loic.local>
2019-03-19 15:38:30 +0100

    Init project

---
+++ NcSejima/NcSejima.h
@@ -0,0 +1,19 @@
+//
+//  NcSejima.h
+//  NcSejima
+//
+//  Created by Loïc GRIFFIE on 19/03/2019.
+//  Copyright © 2019 Loïc GRIFFIE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NcSejima.
+FOUNDATION_EXPORT double NcSejimaVersionNumber;
+
+//! Project version string for NcSejima.
+FOUNDATION_EXPORT const unsigned char NcSejimaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NcSejima/PublicHeader.h>
+
+
---
+++ Podfile
@@ -0,0 +1,14 @@
+platform :ios, '9.1'
+
+target 'NcSejima' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for NcSejima
+
+  target 'NcSejimaTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
