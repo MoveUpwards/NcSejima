169ae5911d329fd17424168e9d21581119a154ea
Loïc GRIFFIE <lgriffie@mbp-de-loic.lan>
2019-11-21 09:54:18 +0100

    Add SPM

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version:5.0
+
+import PackageDescription
+
+let package = Package(
+    name: "NcSejima",
+    platforms: [
+        .iOS(.v10),
+    ],
+    products: [
+        .library(
+            name: "NcSejima",
+            targets: ["NcSejima"]
+        ),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "NcSejima",
+            dependencies: [],
+            path: "NcSejima/Sources"
+        ),
+        .testTarget(
+            name: "NcSejimaTests",
+            dependencies: [],
+            path: "Tests"
+        ),
+    ]
+)
